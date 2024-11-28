324f8529e76b77ca96781e57902e6574190ba64d
Copterrrrr <potsatorn.boon@bumail.net>
2024-11-28 13:47:52 +0700

    --

--- Senior Project/Assets/Scripts/SafeAreaAdjuster.cs
+++ Senior Project/Assets/Scripts/SafeAreaAdjuster.cs
@@ -8,7 +8,7 @@ public class SafeAreaAdjuster : MonoBehaviour
     Rect safeArea;
     Vector2 minAnchor;
     Vector2 maxAnchor;
-/*
+
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -24,7 +24,7 @@ public class SafeAreaAdjuster : MonoBehaviour
         rectTransform.anchorMin = minAnchor;
         rectTransform.anchorMax = maxAnchor;
         
-    }*/
+    }
 }
 
 
